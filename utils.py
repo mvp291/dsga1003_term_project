647b3f96a9a03253c9aa37f8408cd1c00fb8c169
Ma. Elena Villalobos Ponte <villalobos.maelena@gmail.com>
2016-05-05 14:57:42 -0400

    Grid search for model selection

--- utils.py
+++ utils.py
@@ -1,6 +1,17 @@
 import numpy as np
 import pandas as pd
 
+def separate_target(train, val, test, target):
+    X_train, y_train = train.ix[:, :-3], train.ix[:, target]
+    X_val, y_val = val.ix[:, :-3], val.ix[:, target]
+    X_test, y_test = test.ix[:, :-3], test.ix[:, target]
+    return X_train, y_train, X_val, y_val, X_test, y_test
+
+def save_pred_to_file(y_test, y_pred, test, filename='results.csv'):
+    ''' Build dataframe to compare real values with predictions on test set '''
+    new_df = pd.concat([y_test, pd.Series(y_pred, index=y_test.index), test.ix[:, -3:]], axis=1)
+    new_df.to_csv(filename)
+
 
 def loss_azimuth(y_truth, y_predicted):
     dist_upper = abs(y_truth - y_predicted)
