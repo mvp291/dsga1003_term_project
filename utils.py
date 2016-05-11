e9df2d8f5b6fa2b70e0c8ed9a4fc4597687e62e4
Ma. Elena Villalobos Ponte <villalobos.maelena@gmail.com>
2016-05-11 11:12:30 -0400

    All results so far, but messy

--- utils.py
+++ utils.py
@@ -1,6 +1,18 @@
 import numpy as np
 import pandas as pd
 
+
+def get_splitted_dataset(ds):
+    train_ix = [2, 3, 4, 5, 6, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19, 22, 23,
+                25, 26, 27, 28, 31, 33, 34, 35, 36, 37, 39, 40, 41, 42, 43, 44]
+    val_ix = [7, 24, 29, 30, 38]
+    test_ix = [1, 12, 20, 21, 32, 45]
+
+    train = ds[ds.subject_id.isin(train_ix)]
+    val = ds[ds.subject_id.isin(val_ix)]
+    test = ds[ds.subject_id.isin(test_ix)]
+    return train, val, test
+
 def separate_target(train, val, test, target):
     X_train, y_train = train.ix[:, :-3], train.ix[:, target]
     X_val, y_val = val.ix[:, :-3], val.ix[:, target]
